cbad27c60adf0a481033e5099b14095b42fca09b
Ilya <devmakarov@gmail.com>
2019-02-18 01:38:00 +0200

    Update server.js
    no comment

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const app = new Koa
  
 require('./middlewares/logger').init( app )
 require('./middlewares/static').init( app )
-//require('./libs/database')
 require('./middlewares/parser').init( app )
 require('./routes').init( app )
 
